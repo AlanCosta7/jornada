df951901fc59df177547e655b513c038c388cf40
alanpc7 <alanpc7@gmail.com>
2019-12-07 11:23:43 -0300

    final

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,8 @@
 <template>
-  <q-page class="bg-primary column flex flex-center">
-    <img width="100%" class="imgIndex" src="../assets/icons/Jovem.png" alt="">
-    <q-btn to="/login" color="positive" push class="text-white q-ma-xl" label="Entrar"></q-btn>
+  <q-page class="backgroud column flex flex-center">
+    <img width="100px" class="imgIndex" src="../assets/icons/jovem.svg" alt="">
+    <dir class="col-4"></dir>
+    <q-btn to="/login" color="black" outline class=" q-ma-xl" label="Entrar"></q-btn>
   </q-page>
 </template>
 
@@ -10,3 +11,9 @@ export default {
   name: 'PageIndex'
 }
 </script>
+
+<style lang="stylus" scoped>
+  .backgroud {
+      background-image: linear-gradient(to bottom right, $light-blue-14, white);
+  }
+</style>
